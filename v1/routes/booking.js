1135f2e4f0536aa0ef83479c8f65755a3de264ba
Tusarkant Sahoo <trytusar@gmail.com>
2024-05-13 22:13:31 +0530

    Updated booking logic

--- v1/routes/booking.js
+++ v1/routes/booking.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const { createdNewSlot, bookingSlotDownloaded, updateSlot, bookedPuja, temple_under_list_of_slots, bookedList, deleteSlot, getAllTheSlots, TempleUnderAllTheBookings, getSlotsWithBookedData } = require('../controllers/booking.controller');
+const { createdNewSlot, bookingSlotDownloaded, updateSlot, bookedPuja, temple_under_list_of_slots, templeBookedList, deleteSlot, getAllTheSlots, TempleUnderAllTheBookings, getSlotsWithBookedData, userBookedList } = require('../controllers/booking.controller');
 const router = express.Router();
 const authenticate = require('../../middleware/authenticate');
 const TempleAuth = require('../../middleware/temple.auth');
@@ -15,7 +15,8 @@ router.get('/getAlltheSlots', TempleAuth, getAllTheSlots)
 router.put('/updateSlot/:slotId', update_slot_validator, ValidatorResult, TempleAuth, updateSlot);
 router.delete('/deleteSlot/:slotId', delete_slot_validator, ValidatorResult, TempleAuth, deleteSlot)
 router.post('/bookTemplePuja', new_booking_validator, ValidatorResult, authenticate, bookedPuja)
-router.get('/bookedList', authenticate, bookedList);
+router.get('/templeBookedList', TempleAuth, templeBookedList);
+router.get('/userBookedList', authenticate, userBookedList);
 router.get('/templeUnderAlltheBookingList', TempleAuth, TempleUnderAllTheBookings)
 //router.post('/templeAvailableSlots', TempleAuth, getSlotsWithBookedData)
 router.post('/templeAvailableSlots', authenticate, getSlotsWithBookedData)
