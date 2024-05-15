dabbb432e45d48fbe00116fae0d8aae3b5ae80ca
psamantaray77@gmail.com <biplabparida111@gmail.com>
2024-05-15 17:47:17 +0530

    chnagse

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,15 +1,25 @@
+const multer = require('multer');
 
-const multer = require('multer')
+const sanitizeAndRenameFilename = (filename) => {
+  const extension = filename.split('.').pop();
+  const nameWithoutExtension = filename.slice(0, -(extension.length + 1));
 
+  const sanitizedFilename = nameWithoutExtension.replace(/\s+/g, '_')
+                                                .replace(/[^a-zA-Z0-9]/g, '')
+  const timestamp = Date.now();
+  return `${sanitizedFilename}_${timestamp}.${extension}`;
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-      cb(null, 'uploads/') 
+    cb(null, 'uploads/'); 
   },
   filename: function (req, file, cb) {
-      cb(null, file.originalname) 
+    const sanitizedAndRenamedFilename = sanitizeAndRenameFilename(file.originalname);
+    cb(null, sanitizedAndRenamedFilename);
   }
 });
 
 const upload = multer({ storage: storage });
-module.exports = upload
+
+module.exports = upload;
